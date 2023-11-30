ed93e8c5092800f34cc92d21b8c13dbcc1217d43
HN026 <plancksconstant01@gmail.com>
2023-11-30 23:57:23 +0530

    commit

--- main.c
+++ main.c
@@ -4,68 +4,74 @@
 #include <string.h>
 #include "shell.h"
 
-
-// REPL -> Read-Eval-Print-Loop
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
     char *cmd;
+
     do{
         print_prompt1();
         cmd = read_cmd();
+
         if(!cmd){
             exit(EXIT_SUCCESS);
         }
+
         if(cmd[0]=='\0' || strcmp(cmd, "\n")==0)
         {
             free(cmd);
             continue;
         }
-        if(strcmp(cmd, "exit\n") == 0)
-        {
+
+        if(strcmp(cmd, "exit\n")==0){
             free(cmd);
             break;
         }
+
         printf("%s\n", cmd);
-        free(cmd);
     }while(1);
+    
     exit(EXIT_SUCCESS);
 }
 
-char *read_cmd(void){
+char *read_cmd(void)
+{
     char buf[1024];
     char *ptr = NULL;
     char ptrlen = 0;
 
-    while(fgets(buf, 1024, stdin)){
+    while(fgets(buf, 1024, stdin))
+    {
         int buflen = strlen(buf);
+
         if(!ptr){
             ptr = malloc(buflen+1);
         }
         else{
             char *ptr2 = realloc(ptr, ptrlen+buflen+1);
 
-            if(ptr2)
-            {
+            if(ptr2){
                 ptr = ptr2;
             }
-            else
-            {
+            else{
                 free(ptr);
-                ptr=NULL;
+                ptr = NULL;
             }
         }
-        if(!ptr){
+
+        if(!ptr)
+        {
             fprintf(stderr, "error: failed to alloc buffer: %s\n", strerror(errno));
             return NULL;
         }
 
         strcpy(ptr+ptrlen, buf);
+
         if(buf[buflen-1]=='\n'){
-            if(buflen==1 || buf[buflen-2] != '\\'){
-                return ptr;
+            if(buflen==1 || buf[buflen-2]!= '\\'){
+                    return ptr;
             }
+
             ptr[ptrlen+buflen-2] = '\0';
-            buflen -= 2;
+            buflen-=2;
             print_prompt2();
         }
         ptrlen += buflen;
